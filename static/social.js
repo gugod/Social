f3c6d64a6075b2651aaea53395c8ab0db70d7988
Kang-min Liu <gugod@gugod.org>
2009-12-04 21:29:24 +0800

    tweaking the mess a bit

--- static/social.js
+++ static/social.js
@@ -249,7 +249,6 @@ Social.Handlers = {
 $(function() {
     (function() {
         location.hash = "";
-        $("body > *").css("display", "none");
         $("body > *:first").addClass("current");
 
         $(window).load(function() { setTimeout(function() { window.scrollTo(0, 1); }, 10); });
